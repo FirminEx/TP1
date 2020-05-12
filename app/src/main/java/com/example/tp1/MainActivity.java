a0d2d4ec8a9f8701d93b239a7bc404128e2a11da
FirminEx <firminexertier1@gmail.com>
2020-05-12 16:56:16 +0200

    test

---
+++ app/src/main/java/com/example/tp1/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.tp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText bouttonLogin=(EditText) findViewById(R.id.bouttonLogin);
+        String hintLogin=getResources().getString(R.string.demande_login);
+        bouttonLogin.setHint(hintLogin);
+
+        final EditText bouttonMdp=(EditText) findViewById(R.id.bouttonMdp);
+        String hintMdp=getResources().getString(R.string.demande_mdp);
+        bouttonMdp.setHint(hintMdp);
+
+        Button bouttonConnexion=(Button)findViewById(R.id.bouttonConnexion);
+        String hintSeConnecter=getResources().getString(R.string.se_connecter);
+        bouttonConnexion.setText(hintSeConnecter);
+
+        final TextView texteConnexion=(TextView)findViewById(R.id.texteConnexion);
+        String hintConnexion=getResources().getString(R.string.connexion);
+        texteConnexion.setText(hintConnexion);
+
+
+        bouttonConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String login=bouttonLogin.getText().toString();
+                String mdp=bouttonMdp.getText().toString();
+                if(mdp.equals(login)&&mdp.isEmpty()==false){
+                    Intent menu=new Intent(getApplicationContext(),Main2Activity.class);
+                    startActivity(menu);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Mauvais identifiants", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+
+
+
+
+/*public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonHaut=(Button)findViewById(R.id.buttonHaut);
+        Button buttonBas=(Button)findViewById(R.id.buttonBas);
+
+        //Envoyer du text à notre 2e activité
+
+        buttonHaut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent=new Intent(getApplicationContext(),Main2Activity.class);
+                //passer une info à la seconde activité
+                startIntent.putExtra("com.example.a2eappl.qqchose","slt le monde!");
+                startActivity(startIntent);
+            }
+        });
+
+        //Ouvrir une application (google)
+
+        buttonBas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String adresse="http://www.google.com";
+                Uri webadresse=Uri.parse(adresse);
+
+                Intent ouvrirPage=new Intent(Intent.ACTION_VIEW,webadresse);
+                startActivity(ouvrirPage);
+            }
+        });
+    }
+}*/
